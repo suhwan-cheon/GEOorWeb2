b4ada4dad56f4579359b9c47933338f89d6bd84a
suhwan-cheon <soo7652@naver.com>
2022-05-02 14:52:05 +0900

    refact: 코드 경량화

--- src/main/java/geo/hs/controller/HomeController.java
+++ src/main/java/geo/hs/controller/HomeController.java
@@ -3,14 +3,11 @@ package geo.hs.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
 	public String main(){
-
-		return "WFS";
+		return "hsMap";
 	}
 }
